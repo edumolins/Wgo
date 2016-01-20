c86ba7b0d1d582427afe3ae7e900dd4ddccc05db
edumolins <eduard.molins@gmail.com>
2016-01-20 17:05:23 +0100

    Commit 20/01/2016

--- app/src/main/java/wgo_app/wgo/MainActivity.java
+++ app/src/main/java/wgo_app/wgo/MainActivity.java
@@ -5,13 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
+
+    private ViewFlipper viewFlipper;
+    private Button goButton;
+    private float lastX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,8 +71,53 @@ public class MainActivity extends Activity {
             }
         });
 
+        viewFlipper = (ViewFlipper)findViewById(R.id.view_flipper);
+        goButton = (Button)findViewById(R.id.go_button);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
+
+    // Using the following method, we will handle all screen swaps.
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = touchevent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                float currentX = touchevent.getX();
+                // Handling left to right screen swap.
+                if (lastX < currentX) {
+                    // If there aren't any other children, just break.
+                    if (viewFlipper.getDisplayedChild() == 0)
+                    break;
+                    // Next screen comes in from left.
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_left);
+                    // Current screen goes out from right.
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_right);
+                    // Display next screen.
+                    viewFlipper.showNext();
+                }
+                // Handling right to left screen swap.
+                if (lastX > currentX) {
+                    // If there is a child (to the left), kust break.
+                    if (viewFlipper.getDisplayedChild() == 1)
+                    break;
+                    // Next screen comes in from right.
+                    viewFlipper.setInAnimation(this, R.anim.slide_in_from_right);
+                    // Current screen goes out from left.
+                    viewFlipper.setOutAnimation(this, R.anim.slide_out_to_left);
+                    // Display previous screen
+                    viewFlipper.showPrevious();
+                }
+                break;
+        }
+        return false;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
